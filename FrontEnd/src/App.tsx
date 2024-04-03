b5e0c05e825cdf7f9e631087cd34eb92146ad3cf
Satyajeet Kumar Rao <satyajeetrao.rao2@gmail.com>
2024-04-04 00:40:43 +0530

    admin route added at on login

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,12 +1,10 @@
 import { ReactElement } from "react";
 import { AllRoutes } from "./routes/AllRoutes";
-import { Link } from "react-router-dom";
 
 function App(): ReactElement {
   return (
     <>
       <AllRoutes />
-      <Link to={`/property/${1582762}`}>View Property</Link>
     </>
   );
 }
